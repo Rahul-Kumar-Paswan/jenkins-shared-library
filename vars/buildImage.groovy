23e75cc7db5f7257b69c48371f71c3783995cf03
Rahul-Kumar-Paswan <rahulkumarpaswan941+auropro@gmail.com>
2023-05-09 10:17:19 +0530

    second commit

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,11 @@
 #!/usr/bin/env groovy
 
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'server-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t rahulkumarpaswan/demo-app:jma-1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push rahulkumarpaswan/demo-app:jma-1.0'
+    }
 
-import com.example.Docker
-
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
 }
