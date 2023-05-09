5e42dc649e64f12e9bfcedbd73bfbe6619f72e4b
Rahul-Kumar-Paswan <123447842+Rahul-Kumar-Paswan@users.noreply.github.com>
2023-05-09 12:10:53 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "building the docker image..."
+    echo "building the docker image..ghc."
     withCredentials([usernamePassword(credentialsId: 'server-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t rahulkumarpaswan/demo-app:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
